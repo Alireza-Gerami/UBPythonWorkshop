75da57fea64f3e949d5943ae305eddb25ee00dd4
amir-nodehi <amirndh11@gmail.com>
2021-08-04 04:05:18 +0430

    Homework 1 answer added.

---
+++ Session-1/homework/amir-nodehi/2.py
@@ -0,0 +1,7 @@
+print("2 : ")
+a = 2
+b = 5
+print(5 // 2)
+print(5 / 2)
+print(5 * 2)
+
---
+++ Session-1/homework/amir-nodehi/3.py
@@ -0,0 +1,3 @@
+print("3 :")
+print("circumference : " + str((5 * 3.14)) )
+print("side : " + str( ((5/2) ** 2) * 3.14) )
