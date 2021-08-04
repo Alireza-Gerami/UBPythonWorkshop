dfa83af3bd58f40d14a79b8e3c58bca1bc0d43a3
alireza <alireza.gerami4030@gmail.com>
2021-08-04 21:41:45 +0430

    Homework-1 solved fix some problem.

--- Session-1/homework/Answers/2.py
+++ Session-1/homework/Answers/2.py
@@ -4,6 +4,3 @@ c = 2 // 5
 print(c)
 print(5 / 2)
 print(5 * 2)
-
-print(2 // 5)
-print(5.1 // 2)
--- Session-1/homework/Answers/5.py
+++ Session-1/homework/Answers/5.py
@@ -1,7 +1,7 @@
 h, m, s = 5, 42, 35
 
 total_seconds = 0
-total_seconds +=  5 * 60 * 60 
-total_seconds += 46 * 60
-total_seconds += 35
+total_seconds += h * 60 * 60 
+total_seconds += m * 60
+total_seconds += s
 print("total seconds = ", total_seconds)
