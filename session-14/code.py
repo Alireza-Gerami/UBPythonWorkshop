37407668b3379eae47ca5b9a12c05b0ce5068c3e
Esfandiar Pourtahmasb <essipoortahmasb2018@gmail.com>
2021-09-05 00:07:26 +0430

    meet-code

---
+++ session-14/code.py
@@ -0,0 +1,36 @@
+#def print(a, b):
+#    a + b
+#    print(a + b)
+    #None
+
+'''
+def f1(a, b):
+    return lambda a, b: a + b #function
+'''
+
+#x = f1(10, 20)
+#x = lambda a, b: a + b
+#print(x(10, 20))
+
+
+
+#f1 = lambda a, b: print(a + b)
+
+#x = f1(10, 12.5)
+#print(x)
+
+
+
+
+'''
+def f(item):
+    return item[0] + item[1]
+'''
+
+'''
+a = [[3, 1], [1, 100], [1,20]]
+
+a.sort(key = lambda item: item[0] + item[1])
+print(a)
+'''
+
